4266a914d44ca518c3fe349bd5a7ced5baf2a88b
Jhon Castillo <jhona_castillov@soy.sena.edu.co>
2023-09-19 19:28:15 -0500

    sehace la calculadora en JS

---
+++ ejercicios.js
@@ -0,0 +1,24 @@
+// Función para sumar
+function sumar(num1, num2) {
+    return num1 + num2;
+  }
+  
+  // Función para restar
+  function restar(num1, num2) {
+    return num1 - num2;
+  }
+  
+  // Función para multiplicar
+  function multiplicar(num1, num2) {
+    return num1 * num2;
+  }
+  
+  // Función para dividir
+  function dividir(num1, num2) {
+    if (num2 !== 0) {
+      return num1 / num2;
+    } else {
+      return 'No se puede dividir por cero';
+    }
+  }
+  
