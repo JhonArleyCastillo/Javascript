3f06b87d6aa4f611c03aed6675ce402c40c6e1f2
Jhon Arley Castillo Vitovis <128943052+JhonArleyCastillo@users.noreply.github.com>
2023-09-23 16:30:00 -0500

    se hacen ajustes sin prueba1-10.js

--- ejercicios1-10.js
+++ ejercicios1-10.js
@@ -1,96 +1,77 @@
 //ejercicio 1
-//let edad = parseInt(prompt("Introduce tu edad"));
-
-//if (!isNaN(edad)) {
+//let edad = parseInt(prompt("Introduce tu edad"))
 //  if (edad >= 18) {
-    //  alert("Eres mayor de edad. Tu edad es: " + edad);
+    //  alert("Eres mayor de edad. Tu edad es: " + edad)
   //} else if {
-    //alert("Eres menor de edad. Tu edad es: " + edad);
+    //alert("Eres menor de edad. Tu edad es: " + edad)
   //}
 //} else {
-  //alert("Por favor, ingresa un número válido para la edad.");
+  //alert("Por favor, ingresa un número válido para la edad.")
 //}
 
 //ejercicio 2//
 
-/*function calcularPromedio(nota1, nota2, nota3) {
-    let promedio = (nota1 + nota2 + nota3) / 3;
+/*let promedio = (nota1 + nota2 + nota3) / 3;
     return promedio;
-  }
-  
-  function verificaraprobacion(promedio) {
     if (promedio >= 6) {
-      return "El estudiante ha aprobado el curso con un promedio de " + promedio;
+      alert( "El estudiante ha aprobado el curso con un promedio de " + promedio)
     } else {
-      return "El estudiante ha reprobado el curso con un promedio de " + promedio;
+      alert( "El estudiante ha reprobado el curso con un promedio de " + promedio)
     }
-  }
-  
-  let nota1 = parseFloat(prompt("Introduce la primera nota (de 1 a 10):"));
-  let nota2 = parseFloat(prompt("Introduce la segunda nota (de 1 a 10):"));
-  let nota3 = parseFloat(prompt("Introduce la tercera nota (de 1 a 10):"));
-  
-  if (!isNaN(nota1) && !isNaN(nota2) && !isNaN(nota3)) {
-    if (nota1 >= 1 && nota1 <= 10 && nota2 >= 1 && nota2 <= 10 && nota3 >= 1 && nota3 <= 10) {
-      const promedio = calcularPromedio(nota1, nota2, nota3);
-      const mensaje = verificarAprobacion(promedio);
-      alert(mensaje);
-    } else if {
-      alert("Las notas deben estar en el rango de 1 a 10.");
+  let nota1 = parseInt(prompt("Introduce la primera nota (de 1 a 10)"))
+  let nota2 = parseInt(prompt("Introduce la segunda nota (de 1 a 10)"))
+  let nota3 = parseInt(prompt("Introduce la tercera nota (de 1 a 10)"))
+      else if {
+      alert("Las notas deben estar en el rango de 1 a 10.")
     }
-  } else {
-    alert("Por favor, ingresa números válidos para las notas.");
+    else {
+    alert("Por favor, ingresa números válidos para las notas.")
   }
   */
  //ejercicio 3//
 
- /*let numeroa =parseInt(prompt("escriba el numeroA"));  // Puedes cambiar este valor por el que desees
- let numerob =parseInt(prompt("escriba el numeroB"));   // Puedes cambiar este valor por el que desees
- 
+ /*let numeroa =parseInt(prompt("escriba el numeroA"))
+ let numerob =parseInt(prompt("escriba el numeroB"))
  if (numeroa > numerob) {
-   alert('A es mayor que B');
- } else if (numeroa < numerob) {
-   alert('A es menor que B');
- } else {
-   alert('A es igual a B');
+   alert('A es mayor que B')
+ } 
+ else if ('numeroa < numerob') {
+   alert('A es menor que B')
+ } 
+ else {
+   alert('A es igual a B')
   }
  */
 //ejercicio 4//
 
-/*let numeros = [];
-
-// Leer 10 números
+/*const numeros = [];
 for (let i = 0; i < 10; i++) {
-  let numero = parseInt(prompt('Introduce el número {i + 1}:'));
+  const numero = parseInt(prompt('Introduce el número ${i + 1}:'))
   if (!isNaN(numero)) {
-    numeros.push(numero);
+    numeros.push(numero)
   } else {
-    alert('Por favor, ingresa un número válido.');
-    // Si no es un número válido, decrementamos i para repetir la iteración actual
+    alert('Por favor, ingresa un número válido.')
     i--;
   }
 }
-
-// Ordenar los números de forma ascendente
 numeros.sort((a, b) => a - b);
-
-// Mostrar los números de forma ascendente
 alert('Números en orden ascendente: ' + numeros.join(', '));
+
 */
 // ejercicio 5
 
 /*let peso = parseFloat(prompt("Ingresa tu peso en Kg:"));
 
 if (!isNaN(peso)) {
-  if (peso <= 40) {
-    alert("Persona baja de peso");
+  else if (peso <= 40) {
+    alert("Persona baja de peso")
   } else if (peso >= 41 && peso <= 70) {
-    alert("Persona en rango promedio");
-  } else {
-    alert("Persona con sobrepeso");
+    alert("Persona en rango promedio")
+  } else if {
+    alert("Persona con sobrepeso")
   }
 } else {
-  alert("Por favor, ingresa un peso válido.");
+  alert("Por favor, ingresa un peso válido.")
 }
 */
 
@@ -125,8 +106,7 @@ switch (numerodia) {
   default:
     diasemana = "Número de día no válido, por favor ingresa un número entre 1 y 7.";
 }
-
-alert('Día ${numerodia}: ${diaSemana}');
+alert('Día {numerodia}: {diaSemana}');
 */
 // ejercicio 7
 /*let comprat= parseFloat(prompt ("Ingrese el valor total de la compra"))
