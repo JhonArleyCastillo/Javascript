f1c44ea20d780b39e7e6e9d654e33de4e2c7e3ea
Jhon Arley Castillo Vitovis <128943052+JhonArleyCastillo@users.noreply.github.com>
2023-09-22 22:24:34 -0500

    se actualiza falta actualizar 1 a 7

--- ejercicios1-10.js
+++ ejercicios1-10.js
@@ -1,10 +1,10 @@
 //ejercicio 1
-//const edad = parseInt(prompt("Introduce tu edad"));
+//let edad = parseInt(prompt("Introduce tu edad"));
 
 //if (!isNaN(edad)) {
 //  if (edad >= 18) {
     //  alert("Eres mayor de edad. Tu edad es: " + edad);
-  //} else {
+  //} else if {
     //alert("Eres menor de edad. Tu edad es: " + edad);
   //}
 //} else {
@@ -14,11 +14,11 @@
 //ejercicio 2//
 
 /*function calcularPromedio(nota1, nota2, nota3) {
-    const promedio = (nota1 + nota2 + nota3) / 3;
+    let promedio = (nota1 + nota2 + nota3) / 3;
     return promedio;
   }
   
-  function verificarAprobacion(promedio) {
+  function verificaraprobacion(promedio) {
     if (promedio >= 6) {
       return "El estudiante ha aprobado el curso con un promedio de " + promedio;
     } else {
@@ -26,16 +26,16 @@
     }
   }
   
-  const nota1 = parseFloat(prompt("Introduce la primera nota (de 1 a 10):"));
-  const nota2 = parseFloat(prompt("Introduce la segunda nota (de 1 a 10):"));
-  const nota3 = parseFloat(prompt("Introduce la tercera nota (de 1 a 10):"));
+  let nota1 = parseFloat(prompt("Introduce la primera nota (de 1 a 10):"));
+  let nota2 = parseFloat(prompt("Introduce la segunda nota (de 1 a 10):"));
+  let nota3 = parseFloat(prompt("Introduce la tercera nota (de 1 a 10):"));
   
   if (!isNaN(nota1) && !isNaN(nota2) && !isNaN(nota3)) {
     if (nota1 >= 1 && nota1 <= 10 && nota2 >= 1 && nota2 <= 10 && nota3 >= 1 && nota3 <= 10) {
       const promedio = calcularPromedio(nota1, nota2, nota3);
       const mensaje = verificarAprobacion(promedio);
       alert(mensaje);
-    } else {
+    } else if {
       alert("Las notas deben estar en el rango de 1 a 10.");
     }
   } else {
@@ -44,12 +44,12 @@
   */
  //ejercicio 3//
 
- /*const numeroA =parseInt(prompt("escriba el numeroA"));  // Puedes cambiar este valor por el que desees
- const numeroB =parseInt(prompt("escriba el numeroB"));   // Puedes cambiar este valor por el que desees
+ /*let numeroa =parseInt(prompt("escriba el numeroA"));  // Puedes cambiar este valor por el que desees
+ let numerob =parseInt(prompt("escriba el numeroB"));   // Puedes cambiar este valor por el que desees
  
- if (numeroA > numeroB) {
+ if (numeroa > numerob) {
    alert('A es mayor que B');
- } else if (numeroA < numeroB) {
+ } else if (numeroa < numerob) {
    alert('A es menor que B');
  } else {
    alert('A es igual a B');
@@ -57,11 +57,11 @@
  */
 //ejercicio 4//
 
-/*const numeros = [];
+/*let numeros = [];
 
 // Leer 10 números
 for (let i = 0; i < 10; i++) {
-  const numero = parseInt(prompt('Introduce el número ${i + 1}:'));
+  let numero = parseInt(prompt('Introduce el número {i + 1}:'));
   if (!isNaN(numero)) {
     numeros.push(numero);
   } else {
@@ -79,7 +79,7 @@ alert('Números en orden ascendente: ' + numeros.join(', '));
 */
 // ejercicio 5
 
-/*const peso = parseFloat(prompt("Ingresa tu peso en Kg:"));
+/*let peso = parseFloat(prompt("Ingresa tu peso en Kg:"));
 
 if (!isNaN(peso)) {
   if (peso <= 40) {
@@ -96,42 +96,88 @@ if (!isNaN(peso)) {
 
 //ejercicio 6
 
-/*const numeroDia = parseInt(prompt("Introduce un número entre 1 y 7:"));
+/*let numerodia = parseInt(prompt("Introduce un número entre 1 y 7:"));
 
-let diaSemana;
+let diasemana;
 
-switch (numeroDia) {
+switch (numerodia) {
   case 1:
-    diaSemana = "Lunes";
+    diasemana = "Lunes";
     break;
   case 2:
-    diaSemana = "Martes";
+    diasemana = "Martes";
     break;
   case 3:
-    diaSemana = "Miércoles";
+    diasemana = "Miércoles";
     break;
   case 4:
-    diaSemana = "Jueves";
+    diasemana = "Jueves";
     break;
   case 5:
-    diaSemana = "Viernes";
+    diasemana = "Viernes";
     break;
   case 6:
-    diaSemana = "Sábado";
+    diasemana = "Sábado";
     break;
   case 7:
-    diaSemana = "Domingo";
+    diasemana = "Domingo";
     break;
   default:
-    diaSemana = "Número de día no válido, por favor ingresa un número entre 1 y 7.";
+    diasemana = "Número de día no válido, por favor ingresa un número entre 1 y 7.";
 }
 
-alert('Día ${numeroDia}: ${diaSemana}');
+alert('Día ${numerodia}: ${diaSemana}');
 */
 // ejercicio 7
-var comprat= parseFloat(prompt ("Ingrese el valor total de la compra"))
-var descuento;
+/*let comprat= parseFloat(prompt ("Ingrese el valor total de la compra"))
+let descuento;
 if(comprat >= 1000){descuento = comprat * 0.2;}
 else{descuento = comprat * 0.05;}
 var pagototal= comprat - descuento;
-alert("la cantidad a pagar es" + pagototal.toFixed + "pesos");
+alert("la cantidad a pagar es " + pagototal + "pesos");*/
+
+//ejercicio 8
+
+/*let horas = parseInt(prompt("Introduce tus horas laboradas"));
+if (horas <= 40){
+    alert("has ganado $16000")}
+  else {
+    (
+      alert("has ganado $20000")
+    )
+  }*/
+
+// ejercicio 9
+/*let person1n=(prompt("nombre"))
+let person1= parseInt(prompt("edad"))
+let person2n= (prompt("nombre"))
+let person2= parseInt(prompt("edad"))
+if (person1 > person2){
+  alert(person1n + person1 + "es mayor que " + person2n + person2)
+}
+ else{
+  alert(person2n + person2 + " es mayor que" + person1n + person1 )
+ }*/
+
+//ejercicio 10
+
+/*let comprat= parseInt(prompt ("Ingrese el total de camisas de la compra"))
+let descuento;
+if(comprat >= 3){
+  alert("su descuento es del 20%")
+}
+else{
+  (alert("su descuento es del 10 %"))
+}*/
+
+//ejercicio 11
+
+let num1= parseInt(prompt("Ingrese su numero1"))
+let num2= parseInt(prompt("ingrese numero 2"))
+if (num1 = num2){
+  alert (num1*num2)
+}
+else{
+  alert(num1+num2)
+}
+    
